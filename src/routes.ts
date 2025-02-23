8c5a72942fa66568d3883ace6ce01729960dfa32
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2025-02-23 20:05:24 +0000

    Finish

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { Application } from 'express';
 import {authRoutes} from "@root/features/auth/routes/authRoutes";
 import {serverAdapter} from "@services/queues/base.queus";
 
-const BASE_PATH = '/api/v1';
+const BASE_PATH = '/api/v1';ffff
 export default (app: Application)  => {
 
     const routes = () => {
